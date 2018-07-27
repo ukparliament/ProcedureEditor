aea4fdc8a6dc5518c78354bce54336a8ddb72afe
hellikopter <hellikopter@users.noreply.github.com>
2018-07-27 12:26:35 +0100

    redundant using statements

--- Parliament.ProcedureEditor.Web/Api/SolrStatutoryInstrumentController.cs
+++ Parliament.ProcedureEditor.Web/Api/SolrStatutoryInstrumentController.cs
@@ -3,9 +3,6 @@ using Parliament.ProcedureEditor.Web.Api.Configuration;
 using Parliament.ProcedureEditor.Web.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace Parliament.ProcedureEditor.Web.Api
